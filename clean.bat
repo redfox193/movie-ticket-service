cd617de3e0b4bc045f7ce71332a5efb165f085c5
redfox193 <cer2005dima@gmail.com>
2024-04-28 23:14:34 +0300

    fix: update clean.bat

--- clean.bat
+++ clean.bat
@@ -6,4 +6,4 @@ docker rmi iticket-celery:latest
 docker rmi redis:alpine
 docker rmi postgres:13.3
 docker volume rm iticket_kino_db_data
-rm payment_service/data/db.sqlite3
+del payment_service\data\db.sqlite3
