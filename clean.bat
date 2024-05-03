b9067f9126aa021e7811399f02bbb90fba6c49b1
redfox193 <cer2005dima@gmail.com>
2024-05-03 20:53:51 +0300

    update clean.bat

--- clean.bat
+++ clean.bat
@@ -3,6 +3,8 @@ docker rmi iticket-flower:latest
 docker rmi iticket-payment:latest
 docker rmi iticket-kino:latest
 docker rmi iticket-celery:latest
+docler rmi iticket-payment_db_migration:latest
+docker rmi iticket-kino_db_migration:latest
 docker rmi redis:alpine
 docker rmi postgres:13.3
 docker volume rm iticket_kino_db_data
